53988d7f8f96f3ab9b1159a4000d23937628d834
Sean Schlaefli <sean.schlaefli@gmail.com>
2018-03-07 07:41:05 -0800

    Added user id to User table and added comments to explain table definitions.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS User (
+    UserID INT PRIMARY KEY,
     Username VARCHAR(255),
     Password VARCHAR(255),
     FirstName VARCHAR(255),
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 CREATE TABLE IF NOT EXISTS Video (
-    VideoID INT PRIMARY Key,
+    VideoID INT PRIMARY KEY,
     NumFrames INT, 
     FramesPerSecond INT, 
     Width INT,
@@ -17,19 +18,32 @@ CREATE TABLE IF NOT EXISTS Video (
     --DataID INT REFERENCES OpenFaceData
 );
 
+
+/*
+the database assignment specifies that we will be storing eye points
+acquired from OpenFace and Fabian Timm's algorithm (4 points total). 
+although the assignments do not specify using OpenFace to get these 
+points so we will only store points from FT's algorithm
+*/
 CREATE TABLE IF NOT EXISTS Frame (
-    FrameID INT PRIMARY Key,
-    PupilRightX REAL,
-    PupilRightY REAL,
-    PupilLeftX REAL,
-    PupilLeftY REAL,
+    FrameID INT PRIMARY KEY,
+    FTPupilRightX REAL,
+    FTPupilRightY REAL,
+    FTPupilLeftX REAL,
+    FTPupilLeftY REAL,
     Roll REAL,
     Pitch REAL,
     Yaw REAL
 );
 
+/*
+the key for open face data should be which point
+it is and the FrameID associated with that point.
+recall that open face will return 68 points if a face
+is found in an image 
+*/
 CREATE TABLE IF NOT EXISTS OpenFaceData (
-    DataID INT PRIMARY Key,
+    DataID INT PRIMARY KEY,
     X REAL,
     Y REAL
 );
