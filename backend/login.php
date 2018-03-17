566102ce8e7c0e3b52145c0b902472f7e2758e36
Sean Schlaefli <sean.schlaefli@gmail.com>
2018-03-17 14:44:19 -0700

    Converted tabs to spaces

--- backend/login.php
+++ backend/login.php
@@ -2,14 +2,14 @@
 session_start();
 
 function end_session() {
-	session_unset();
-	session_destroy();
+    session_unset();
+    session_destroy();
 }
 
 function exit_script() {
-	end_session();
-	echo 0;
-	exit;
+    end_session();
+    echo 0;
+    exit;
 }
 
 ?>
@@ -20,22 +20,22 @@ include("database_handler.php");
 if (!isset($_POST['username']) 
     ||
     !isset($_POST['password']))  {
-	exit_script();
+    exit_script();
 } 
 
 date_default_timezone_set("America/Los_Angeles");
 
 $conn = connect();
 if (is_null($conn)) {
-	echo 'Failed to connect to database' . "\n";
-	exit_script();
+    echo 'Failed to connect to database' . "\n";
+    exit_script();
 }
 
 $username = $_POST['username'];
 $password = $_POST['password'];
  
 if (!(verify_login($conn, $username, $password))) {
-	exit_script();
+    exit_script();
 }
 
 // could store the id from the db to pass to other php scripts
@@ -50,5 +50,5 @@ update_after_login($conn, $username, $ip, $time);
 // echo the session back to client
 // if client receives a session, then login was successful
 // load user account page
-echo $session . "\n";	
+echo $session . "\n";   
 ?>
