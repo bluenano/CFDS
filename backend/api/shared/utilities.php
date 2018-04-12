b37d1dfe8eaa38830d1f7048ebcab10a8d54938e
Sean Schlaefli <sean.schlaefli@gmail.com>
2018-04-12 12:58:56 -0700

    Included utility file.

--- backend/api/shared/utilities.php
+++ backend/api/shared/utilities.php
@@ -1,6 +1,9 @@
 <?php
 // utility constants and functions to be used in our project
 
+define ('SITE_ROOT', '~/Sites/cs160/test');
+
+
 function exit_script_on_failure($message) {
 	echo json_encode(array('message' => $message));
 	exit;
