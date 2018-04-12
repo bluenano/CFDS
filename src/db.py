1310a071f28a62b265a2a82c4ceb60d93abab3ba
0xED5 <psppowered@gmail.com>
2018-04-11 18:03:30 -0700

    added more relevant libpqxx test

--- src/db.py
+++ src/db.py
@@ -12,21 +12,21 @@ height = 1080
 frame_rate = 30
 frame_count = 120
 
-username = 'user'
-password = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-firstname = 'user'
-lastname = 'user'
-lastip = '127.0.0.1'
-lastlogin = datetime.datetime.now()
-sessionid = 'abcdabcdabcdabcdabcdabcdabcdabcd'
+#username = 'user'
+#password = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+#firstname = 'user'
+#lastname = 'user'
+#lastip = '127.0.0.1'
+#lastlogin = datetime.datetime.now()
+#sessionid = 'abcdabcdabcdabcdabcdabcdabcdabcd'
 
-testUser = '''INSERT INTO userinfo (Username, Password, FirstName, LastName, LastIp, LastLogin, SessionID) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING UserID;'''
-testArg = username, password, firstname, lastname, lastip, lastlogin, sessionid
-cur.execute(testUser, testArg)
-user_id = str(cur.fetchone()[0])
-print(user_id)
+#testUser = '''INSERT INTO userinfo (Username, Password, FirstName, LastName, LastIp, LastLogin, SessionID) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING UserID;'''
+#testArg = username, password, firstname, lastname, lastip, lastlogin, sessionid
+#cur.execute(testUser, testArg)
+#user_id = str(cur.fetchone()[0])
+#print(user_id)
 
-conn.commit()
+#conn.commit()
 
 sql = '''INSERT INTO Video (UserID, NumFrames, FramesPerSecond, Width, Height)
     VALUES (%s, %s, %s, %s, %s) RETURNING VideoID;'''
