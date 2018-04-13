7fc37049d6d58af92fa7d837a69d4baa881b25a7
0xED5 <psppowered@gmail.com>
2018-04-12 22:59:16 -0700

    parly fixed cpp database test program

--- src/db.cpp
+++ src/db.cpp
@@ -1,61 +1,73 @@
 #include <iostream>
 #include <pqxx/pqxx>
 
-//typedef dlib::point lmCoord;//landmark coordinate pair
+/*
+    These are gathered now also; once at the start of the process.
 
-//typedef lmCoord image_coords_xy;
+    Again... not sure what do do with them...
+*/
+struct VideoMetadata
+{
+    int32_t nframes;
+    int32_t fps;
+    int32_t width;
+    int32_t height;
+};
 
 struct alignas(4) PairInt16
 {
     int16_t x16, y16;//2 bytes each
 
-    int16_t x() const {return x16;}
+    int16_t x() const {return x16;}//dumb getters, was trying to fit it into dlib
     int16_t y() const {return y16;}
 };
 
-//typedef dlib::point lmCoord;//landmark coordinate pair
-
-//typedef xy_coords_int16 lmCoord;
 
 struct EulerAnglesF32
 {
     float yaw, pitch, roll;//4 bytes each
 };
 
-//note error conditions
+/*
+    One of these is filled in each frame.
+
+    Some fields will be set to error conditions or
+    unfilled/stale data if detection failed, see below.
+*/
 struct FrameResults//a POD struct
 {
-    PairInt16 marks68[68];//if marks68[0].x==-1 then was an error, known where found (no pupils either)
+    PairInt16 marks68[68];  //if marks68[0].x==-1 then there was no detection,
+                            //and nothing else is filled in for this entire object
 
-    PairInt16 left_pupil;//if .x==-1 not found
-    PairInt16 right_pupil;//if .x==-1 not found
-    //70 * 4
-    EulerAnglesF32 rot;//+ 4*3 //if marks found, always found
+    PairInt16 left_pupil;   //if .x==-1 then not found
+    PairInt16 right_pupil;  //if .x==-1 then not found
+
+    EulerAnglesF32 rotation;//if marks68 found, then these will be filled in
 };
 
 int main(int, char *argv[])
 {
   pqxx::connection c("user=postgres host=localhost password=asdf dbname=cs160");
 
-  FrameResults* p_test = new FrameResults;
-  FrameResults test = *p_test;
+  //FrameResults* p_test = new FrameResults;
+  FrameResults test;//= *p_test;
 
-  /*int count = 1;
+  int count = 1;
   for (int i=0;i<68;i++) {
     test.marks68[i].x = count;
     count++;
     test.marks68[i].y = count;
     count++;
-  }*/
+  }
 
   test.left_pupil.x16 = 69;
   test.left_pupil.y16 = 70;
   test.right_pupil.x16 = 71;
   test.right_pupil.y16 = 72;
 
-  test.rot.yaw = 1.11;
-  test.rot.pitch = 2.22;
-  test.rot.roll = 3.33;
+  test.rotation.yaw = 1.11;
+  test.rotation.pitch = 2.22;
+  test.rotation.roll = 3.33;
 
 
   pqxx::work txn(c);
@@ -76,13 +88,13 @@ int main(int, char *argv[])
     txn.quote(test.right_pupil.y16) + ", " +
     txn.quote(test.left_pupil.x16) + ", " +
     txn.quote(test.right_pupil.x16) + ", " +
-    txn.quote(test.rot.roll) + ", " +
-    txn.quote(test.rot.pitch) + ", " +
-    txn.quote(test.rot.yaw) +
+    txn.quote(test.rotation.roll) + ", " +
+    txn.quote(test.rotation.pitch) + ", " +
+    txn.quote(test.rotation.yaw) +
     ")"
   );
 
-  /*pqxx::result r2 = txn.exec(
+  pqxx::result r2 = txn.exec(
     "INSERT INTO frame("
     "videoid, "
     "ftpupilrightx, "
@@ -96,13 +108,9 @@ int main(int, char *argv[])
     txn.quote(4) + ", " +
     txn.quote(test.right_pupil.x16) + ", " +
     txn.quote(test.right_pupil.y16) + ", " +
-    txn.quote(test.left_pupil.x16) + ", " +
-    txn.quote(test.right_pupil.x16) + ", " +
-    txn.quote(test.rot.roll) + ", " +
-    txn.quote(test.rot.pitch) + ", " +
-    txn.quote(test.rot.yaw) +
+    txn.quote(test.left_pupil.x16) +
     ")"
-  );*/
+  );
 
   /*pqxx::result r = txn.exec(
     "SELECT width "
