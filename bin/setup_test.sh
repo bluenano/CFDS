1e263ecd40b9f220c7c3dd65be31cf3122bdfcac
Sean Schlaefli <sean.schlaefli@gmail.com>
2018-04-18 16:06:00 -0700

    Modified script to remove redundant commands and change how permissions are set in the site directory.

--- bin/setup_test.sh
+++ bin/setup_test.sh
@@ -2,7 +2,7 @@
 SITE=
 PROJECT=
 
-sudo rm -r $SITE/test/api
+sudo rm -r $SITE/test
 mkdir $SITE/test/
 mkdir $SITE/test/api
 mkdir $SITE/test/api/config
@@ -12,25 +12,16 @@ mkdir $SITE/test/api/shared
 mkdir $SITE/uploads
 sudo chgrp -R _www $SITE/uploads
 sudo chmod u=rwx,g=rwx $SITE/uploads
-
-# clear the directories, mkdirs will fail if
-# initial structure already exists
-rm -r $SITE/test/*.html
-rm -r $SITE/test/*.php
-rm -r $SITE/test/*.js
-rm -r $SITE/test/*.css
+sudo chmod g=rwx $SITE/test/api/scripts
 
 cp $PROJECT/Front-End/*.html $SITE/test
 cp $PROJECT/Front-End/*.js $SITE/test
 cp $PROJECT/Front-End/*.css $SITE/test
 
 cp $PROJECT/api/config/*.php $SITE/test/api/config
-#cp ../backend/api/config/config.php ~/Sites
 cp $PROJECT/api/video/*.php $SITE/test/api/video
 cp $PROJECT/api/scripts/*.php $SITE/test/api/scripts
 cp $PROJECT/api/shared/*.php $SITE/test/api/shared
 mv $SITE/test/index.html $SITE/test/login.html
 cp $SITE/database.ini $SITE/test/api/config
 mv $SITE/test/api/config/config.php $SITE
-
-chmod -R g=rwx $SITE/test
